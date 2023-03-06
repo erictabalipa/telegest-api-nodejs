27caa42eb5278d380fe994f300ea186b789d0833
Eric Tabalipa <eric.tabalipa@pinho.com.br>
2023-03-06 11:40:26 -0300

    fix CORS

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', ' Authorization, Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
